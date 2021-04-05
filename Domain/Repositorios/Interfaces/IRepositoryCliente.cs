6cf660e8e085db8605159c4590d9584e68ec542d
vagner <vagner@techdog.com.br>
2021-04-04 22:32:52 -0300

    Criando repositorios

---
+++ Domain/Repositorios/Interfaces/IRepositoryCliente.cs
@@ -0,0 +1,14 @@
+﻿using AmandaStore.Domain.Entitys;
+using AmandaStore.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AmandaStore.Domain.Repositorios.Interfaces
+{
+    public interface IRepositoryCliente: IRepository<Cliente>
+    {
+        void AdicionarCliente(Cliente cliente);
+    }
+}
---
+++ Domain/Repositorios/Interfaces/IRepositoryUsuario.cs
@@ -0,0 +1,14 @@
+﻿using AmandaStore.Domain.Entitys;
+using AmandaStore.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AmandaStore.Domain.Repositorios.Interfaces
+{
+    interface IRepositoryUsuario: IRepository<Usuario>
+    {
+        void AdicionarUsuario(Usuario usuario);
+    }
+}
